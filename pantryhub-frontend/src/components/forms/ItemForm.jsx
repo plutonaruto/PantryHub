eebea3375d046df3f58f81e4af4cb2594e989296
plutonaruto <charistan297@gmail.com>
2025-05-27 15:49:23 +0800

    Add image input to Inventory form

--- pantryhub-frontend/src/components/forms/ItemForm.jsx
+++ pantryhub-frontend/src/components/forms/ItemForm.jsx
@@ -1,12 +1,55 @@
 import TextInput from "../shared/TextInput";
 
-export default function ItemForm({formData, onChange, onSubmit}) {
+export default function ItemForm({formData, onChange, onSubmit, onImageChange}) {
     return (
         <form onSubmit={onSubmit} className="space-y-3 mb-6"> 
-            <TextInput label="Name" name="name" value={formData.name} onChange={onChange} required />
-            <TextInput label="Expiry Date" name="expiry" value={formData.expiry} onChange={onChange} type="date" required />
-            <TextInput label="Quantity" name="quantity" value={formData.quantity} onChange={onChange} type="number"required />
-            <button type="submit" className="bg-purple-600 text-white px-4 py-2 rounded">Add to Inventory</button>
+            <div>
+                <TextInput 
+                    label="Name" 
+                    name="name" 
+                    value={formData.name} 
+                    onChange={onChange} 
+                    required 
+                />
+            </div>
+            
+            <div>
+                <TextInput 
+                    label="Expiry Date" 
+                    name="expiry" 
+                    value={formData.expiry} 
+                    onChange={onChange} 
+                    type="date" 
+                    required 
+                />
+            </div>
+            
+            <div>
+                <TextInput 
+                    label="Quantity" 
+                    name="quantity" 
+                    value={formData.quantity} 
+                    onChange={onChange} 
+                    type="number"
+                    required 
+                />
+            </div> 
+
+            <div>
+                <input
+                type="file"
+                accept="image/*"
+                onChange={onImageChange}
+                className="block mt-1"
+                />
+            </div>
+            
+            <button 
+                type="submit" 
+                className="bg-purple-600 text-white px-4 py-2 rounded"
+            >
+                Add to Inventory
+            </button>
         </form>
     );
 }
