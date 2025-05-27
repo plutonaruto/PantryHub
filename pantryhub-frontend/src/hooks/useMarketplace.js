45fa4dfa64fadd6a200b7b935761a4e50426bd77
plutonaruto <charistan297@gmail.com>
2025-05-27 14:11:49 +0800

    Update claim logic to support claiming of multiple quantities

--- pantryhub-frontend/src/hooks/useMarketplace.js
+++ pantryhub-frontend/src/hooks/useMarketplace.js
@@ -2,21 +2,28 @@ import { useState } from 'react';
 
 export function useMarketplace() {
   const [items, setItems] = useState([]);
-  const [formData, setFormData] = useState({ name: '', description: '', expiry: '' });
+  const [formData, setFormData] = useState({ name: '', description: '', expiry: '' , quantity: 1}); //default quantity that can be changed
 
   const addItem = (e) => {
     e.preventDefault();
-    setItems([...items, formData]);
-    setFormData({ name: '', description: '', expiry: '' });
+    const newItem = { 
+      ...formData, 
+      quantity: parseInt(formData.quantity, 10)
+    };
+    setItems([...items, newItem]);
+    setFormData({ name: '', description: '', expiry: '', quantity: 1});
   };
 
   const updateForm = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  const claimItem = (index) => {
+  const claimItem = (index, quantityToClaim) => {
     const updated = [...items];
-    updated.splice(index, 1); // remove the item
+    updated[index].quantity -= quantityToClaim;
+    if (updated[index].quantity <= 0) {
+      updated.splice(index, 1); // remove the item if none left
+    }
     setItems(updated);
   };
 
