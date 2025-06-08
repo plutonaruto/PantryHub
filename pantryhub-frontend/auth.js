5963a4f3271c2b89010bd9f920ac9424f4f46a08
jaydeeen <jadyncheng@gmail.com>
2025-06-08 23:26:12 +0800

    Add files via upload

---
+++ pantryhub-frontend/auth.js
@@ -0,0 +1,9 @@
+import { auth } from "../firebase";
+
+export async function storeFirebaseToken() {
+    if (!auth.currentUser) 
+        return ;
+    const idToken = await auth.currentUser.getIdToken(true);
+    localStorage.setItem("firebaseToken", idToken);
+}
+
