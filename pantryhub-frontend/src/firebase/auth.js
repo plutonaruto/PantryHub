a56412d3d2c54824e035326ad55c55686d54f392
jaydeeen <jadyncheng@gmail.com>
2025-06-10 00:33:11 +0800

    Update auth.js

--- pantryhub-frontend/src/firebase/auth.js
+++ pantryhub-frontend/src/firebase/auth.js
@@ -1,4 +1,5 @@
 import { auth } from "./firebase";
+import jwtDecode from 'jwt-decode';
 
 export async function storeFirebaseToken() {
     if (!auth.currentUser) 
@@ -6,4 +7,14 @@ export async function storeFirebaseToken() {
     const idToken = await auth.currentUser.getIdToken(true);
     localStorage.setItem("firebaseToken", idToken);
 }
+
+const token = localStorage.getItem('token');
+let userRole = null;
+
+if (token) {
+  const decoded = jwtDecode(token);
+  userRole = decoded.role;
+}
+
+export const getUserRole = () => userRole;
 
